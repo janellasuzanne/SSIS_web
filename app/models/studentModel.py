71ff9af7d0ff17ee1926f9294a4fa9f8ba635295
Janella Suzanne Balantac <janellasuzanne.balantac@g.msuiit.edu.ph>
2024-08-07 20:00:12 +0800

    Create Student Single Get & Delete MySQL Methods

--- app/models/studentModel.py
+++ app/models/studentModel.py
@@ -6,12 +6,26 @@ class StudentModel:
     def get_students(cls):
         try:
             cur = mysql.connection.cursor()
-            sql = '''SELECT * FROM student ORDER BY student_id ASC'''
-            cur.execute(sql)
-            student = cur.fetchall()
-            return student
+            cur.execute(
+                "SELECT * FROM `student` ORDER BY `student_id` ASC"
+            )
+            students = cur.fetchall()
+            return students
         except Exception as e:
             return f"Failed to load Student List: {str(e)}"
+        
+    @classmethod
+    def get_single_student(cls, id):
+        try:
+            cur = mysql.connection.cursor()
+            cur.execute(
+                "SELECT * FROM `student` WHERE student_id = %s",
+                (id,)
+            )
+            mysql.connection.commit()
+            return "Student fetched successfully!"
+        except Exception as e:
+            return f"Failed to fetch student: {str(e)}"
         
     @classmethod
     def add_student(cls, id, firstname, lastname, year, gender, course):
@@ -19,9 +33,22 @@ class StudentModel:
             cur = mysql.connection.cursor()
             cur.execute(
                 "INSERT INTO `student` (`student_id`, `firstname`, `lastname`, `year`, `gender`, `course_id`) VALUES (%s, %s, %s, %s, %s, %s)",
-                (id, firstname, lastname, year, gender, course),
+                (id, firstname, lastname, year, gender, course,)
             )
             mysql.connection.commit()
-            return "Faculty created successfully!"
+            return "Student created successfully!"
         except Exception as e:
             return f"Failed to create College: {str(e)}"
+        
+    @classmethod
+    def delete_student(cls, id):
+        try:
+            cur = mysql.connection.cursor()
+            cur.execute(
+                "DELETE FROM `student` WHERE student_id = %s",
+                (id,)
+            )
+            mysql.connection.commit()
+            return "Student deleted successfully!"
+        except Exception as e:
+            return f"Failed to delete student: {str(e)}"
